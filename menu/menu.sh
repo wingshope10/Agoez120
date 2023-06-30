787fe765c7f20da8560ad47df89ae893ede635fa
wingshope10 <137362720+wingshope10@users.noreply.github.com>
2023-06-30 23:01:11 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -196,7 +196,7 @@ DATE=$(date +'%d %B %Y')
 datediff() {
     d1=$(date -d "$1" +%s)
     d2=$(date -d "$2" +%s)
-    echo -e "  ${BIPurple}│$NC  Expiry In     : $(( (d1 - d2) / 86400 )) Days $NC"
+    echo -e "     ${BIRed}│$NC  Expiry In     : $(( (d1 - d2) / 86400 )) Days $NC"
 }
 mai="datediff "$Exp" "$DATE""
 echo -e " ${BIRed} ┌─────────────────────────────────────────────────────┐${NC}"

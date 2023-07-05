e8c23c6fd6e9ca1189fe25ff4548a8089d5d9edd
wingshope10 <137362720+wingshope10@users.noreply.github.com>
2023-07-05 11:43:49 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -205,7 +205,7 @@ echo -e "   ${BIRed} [${BIGreen}10${BIRed}]${GREEN}INSTAL UDP ${NC}"
 echo -e "   ${BIRed} [${BIGreen}x${BIRed}]${GREEN} EXIT ${NC}"  
 echo -e "${BIRed}  └─────────────────────────────────────────────────────┘${NC}"
  echo -e "${BIRed}  ┌─────────────────────────────────────────────────────┐${NC}" 
- echo -e "${BIRed}  │   ${BIGreen}    HARI ini${NC}: ${red}$ttoday$NC ${BIGreen}KEMARIN${NC}: ${red}$tyest$NC ${BIGreen}BULAN${NC}: ${red}$tmon$NC $NC" 
+ echo -e "${BIRed}  │   ${BIGreen}   HARI ini${NC}: ${red}$ttoday$NC ${BIGreen}KEMARIN${NC}: ${red}$tyest$NC ${BIGreen}BULAN${NC}: ${red}$tmon$NC $NC" 
  echo -e "${BIRed}  └─────────────────────────────────────────────────────┘${NC}"
 DATE=$(date +'%d %B %Y')
 datediff() {
